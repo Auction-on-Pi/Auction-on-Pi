a15d259ee6c2e5076349f8eca4c0b3a0eb832e69
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-06 15:04:00 +0200

    Update environments.ts

--- backend/src/environments.ts
+++ backend/src/environments.ts
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
 
-console.log("NODE_ENV: " + process.env.NODE_ENV);
-
 dotenv.config();
+
+export default {
+  database_url: process.env.DATABASE_URL,
+  frontend_url: process.env.FRONTEND_URL || 'https://auction-on-pi-frontend.vercel.app',
+  session_secret: process.env.SESSION_SECRET || 'your-secret-key',
+};
