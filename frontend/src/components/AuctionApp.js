8ebdc638a7978eff9a65e69dcc6686c1edd67c19
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-06 12:15:44 +0200

    Create AuctionApp.js

---
+++ frontend/src/components/AuctionApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AuctionApp() {
+  return (
+    <div>
+      <h1>Auction-on-Pi</h1>
+      <p>Your auction app is ready!</p>
+    </div>
+  );
+}
+
+export default AuctionApp;
