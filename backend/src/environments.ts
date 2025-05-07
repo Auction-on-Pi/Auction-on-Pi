4ef77f590095a097a1a7ecb346acd8dadce140fe
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-07 12:47:35 +0200

    Update environments.ts

--- backend/src/environments.ts
+++ backend/src/environments.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  database_url: process.env.DATABASE_URL || '',
-  frontend_url: process.env.FRONTEND_URL || 'https://auction-on-pi-frontend.vercel.app',
-  session_secret: process.env.SESSION_SECRET || 'your-secret-key',
+  database_url: process.env.DATABASE_URL || 'postgresql://postgres.cguwsbdsweikvbnwziaw:p7LdtbAGz2L2vC76@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres',
+  frontend_url: process.env.FRONTEND_URL || 'https://auction-on-pi.vercel.app',
+  session_secret: process.env.SESSION_SECRET || 'mysecrect123',
   platform_api_url: process.env.PLATFORM_API_URL || 'https://api.minepi.com',
-  pi_api_key: process.env.PI_API_KEY || ''
+  pi_api_key: process.env.PI_API_KEY || 'https://api.minepi.com'
 };
