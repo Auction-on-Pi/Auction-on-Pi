7f7db86de992cd6a3707f8fa4832764d06c7db84
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-07 14:22:08 +0200

    Update session.ts

--- backend/src/types/session.ts
+++ backend/src/types/session.ts
@@ -1,8 +1,9 @@
 import { Session, SessionData } from 'express-session';
+import { PrismaClient } from '@prisma/client';
 
 declare module 'express' {
   interface Request {
     session: Session & Partial<SessionData> & { user?: { id: number; piUserId: string; username: string } };
-    prisma: any; // Temporary fix for Prisma type
+    prisma: PrismaClient;
   }
 }
