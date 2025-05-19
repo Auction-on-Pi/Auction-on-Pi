3100c08cd0e44307c19ec5db71518dd2ac244649
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-19 22:59:03 +0200

    Update connect-pg-simple.d.ts

--- backend/src/types/connect-pg-simple.d.ts
+++ backend/src/types/connect-pg-simple.d.ts
@@ -1,9 +1,14 @@
-// Temporary type declaration until the package gets properly installed
 declare module 'connect-pg-simple' {
   import { Store } from 'express-session';
   import { Pool, PoolConfig } from 'pg';
 
-  function pgSimple(session: any): new (options?: any) => Store;
+  function pgSimple(session: any): new (options?: {
+    pool?: Pool;
+    tableName?: string;
+    createTableIfMissing?: boolean;
+    ttl?: number;
+    pruneSessionInterval?: number;
+  }) => Store;
 
   export = pgSimple;
 }
