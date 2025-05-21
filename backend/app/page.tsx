b2f30d4eb005ad54e6adf2aa39f7853a6fa55c3a
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-21 21:11:02 +0200

    Delete backend directory

--- backend/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Auction-on-Pi Backend</h1>
-      <p>Welcome to the backend API.</p>
-    </div>
-  );
-}
--- backend/src/types/index.ds.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="./connect-pg-simple.d.ts" />
-/// <reference path="./session.d.ts" />
