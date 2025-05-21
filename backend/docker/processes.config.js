f073544c6ca9a690f6825cffe4becee05792ba2d
LewisZett <88579205+Auction-on-Pi@users.noreply.github.com>
2025-05-21 15:09:35 +0200

    Update processes.config.js

--- backend/docker/processes.config.js
+++ backend/docker/processes.config.js
@@ -1,11 +1,13 @@
-// PM2 config file to run the app in production mode.
-// Make sure the name of this file ends with config.js
-
 module.exports = {
-  apps: [{
-    name: "demoapp-backend",
-    script: "/usr/src/app/build/index.js",
-    exec_mode: "cluster",
-    instances: 4,
-  }]
-}
+  apps: [
+    {
+      name: 'auction-on-pi-backend',
+      script: 'dist/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
